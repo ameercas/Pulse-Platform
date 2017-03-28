0a163e54b1afc6af7c3388b79388c4005d75b97e
sem-nowsquare <s.kokhuis@nowsquare.com>
2017-03-28 13:15:51 +0200

    Added bank account and name config for confirmation screen and order mail

--- local/app/config/payment-gateways.php
+++ local/app/config/payment-gateways.php
@@ -58,7 +58,8 @@ return array(
 		*/
 
 		'bank' => array(
-			'active' => true
+			'active' => true,
+			'account' => 'XXXX.XXXX.XXXX Attn BUSINESS NAME' /* Enter your bank account number and name here */
 		),
 
 		/*
