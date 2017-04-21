f9f7930a3ada74f2a2751e12637aa3b4c2bae189
sem-nowsquare <s.kokhuis@nowsquare.com>
2017-04-21 12:00:38 +0200

    Facebook widget updated http://findmyfacebookid.com to https://findmyfbid.com

--- widgets/facebook/lang/nl/global.php
+++ widgets/facebook/lang/nl/global.php
@@ -3,7 +3,7 @@
 return array(
 	"name" => "Facebook",
 	"facebook_id" => "Facebook bedrijfspagina ID",
-	"facebook_id_help" => "Je Facebook ID is het laatste deel van uw URL. U kunt ook gebruik maken van de numerieke id die u hier kunt vinden <a href='http://findmyfacebookid.com/' target='_blank'>http://findmyfacebookid.com/</a>.",
+	"facebook_id_help" => "Je Facebook ID is het laatste deel van uw URL. U kunt ook gebruik maken van de numerieke id die u hier kunt vinden <a href='https://findmyfbid.com/' target='_blank'>https://findmyfbid.com/</a>.",
 	"enter_facebook_id" => "Voer een Facebook ID in",
 	"show_title" => "Laat de titel zien",
 	"max_posts" => "Maximum aantal berichten",
